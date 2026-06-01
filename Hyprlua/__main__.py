b142491830d7816e512f21e7067ec4a51064abc8
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-06-01 21:49:45 +0200

    Inital cli interface

--- Hyprlua/__main__.py
+++ Hyprlua/__main__.py
@@ -1,26 +1,77 @@
-#from pathlib import Path
-from parser import ConfigParser
+import parser
 import config_file
+import argparse
 
-def ask_home():
-    conf = config_file.conf()
+
+def show_help():
+    print('''
+    === HyprLua Config Manager ===
     
-    answer = input('Use {} as config path? (y/n):'.format(conf.conf_dir))
+    Usage: hyprlua [command] [options]
     
-    if answer.lower() == "n":
-        new_conf = input('Enter custom path to hyprland config dir:')
-        
-        conf = config_file.conf(new_conf)
+    Commands:
+      check          Check hyprlua configuration
+      init           Initialize with auto-detection
+      config [path]  View/parse configuration file
+    
+    Options:
+      --help, -h     Show this help message
+      --version, -v  Show version information
+    ''')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='HyprLua Configuration Manager')
+    parser.add_argument('--help', '-h', action='store_true', help='Show help and exit')
+    parser.add_argument('--version', '-v', action='version', version='HyprLua 0.1')
+    parser.add_argument('--config', '-c', dest='config_path', nargs='?', const='.', default=None,
+                       help='Path to config directory (uses current dir if omitted)')
+    parser.add_argument('command', nargs='?', default='check', choices=['check', 'init', 'config'],
+                       help='Command to execute (default: check)')
+    parser.add_argument('config_file', nargs='?', default=None, help='Path to config file (for config command)')
+    
+    return parser.parse_args()
+
+
+def get_conf_dir(config_path=None):
+    """Get configuration directory from CLI argument or auto-detection."""
+    if config_path:
+        conf = config_file.conf(config_path)
+    else:
+        conf = config_file.conf()
     
     return conf
-        
 
 
-
-# Ask for conf dir
-conf = ask_home()
-
-print('Now using {} as conf dir'.format(conf.conf_dir))
+def main():
+    args = parse_arguments()
     
-parser = ConfigParser(config_dir=conf.conf_dir)
-config = parser.parse()
+    if args.help:
+        show_help()
+        return
+    
+    # Get config directory from CLI argument or auto-detect
+    conf = get_conf_dir(config_path=args.config_path)
+    print('Using {} as config directory'.format(conf.conf_dir))
+    
+    if args.command == 'check':
+        parser_args = parser.ConfigParser(config_dir=conf.conf_dir)
+        config = parser_args.parse()
+        print('Configuration loaded successfully!')
+    
+    elif args.command == 'init':
+        print('Initialized using {} as conf dir'.format(conf.conf_dir))
+    
+    elif args.command == 'config':
+        if args.config_file:
+            print('Parsing config from: {}'.format(args.config_file))
+            parser_args = parser.ConfigParser(config_dir=conf.conf_dir)
+            config = parser_args.parse()
+        else:
+            parser_args = parser.ConfigParser(config_dir=conf.conf_dir)
+            config = parser_args.parse()
+            print('Configuration file: {}'.format(config_file.conf().conf_file))
+
+
+if __name__ == '__main__':
+    main()

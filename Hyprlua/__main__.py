e2ae3cb3f994d837a46d9bcd8367472ba3d91a79
moonlitmarigold <moonlitmarigold12@gmail.com>
2026-06-01 22:48:46 +0200

    Updated cli interface

--- Hyprlua/__main__.py
+++ Hyprlua/__main__.py
@@ -1,47 +1,33 @@
-import parser
-import config_file
+from . import config_file
 import argparse
 
 
 def show_help():
     print('''
-    === HyprLua Config Manager ===
+    =======  HyprLua  =======
     
-    Usage: hyprlua [command] [options]
+    Usage: hyprlua [options]
     
-    Commands:
-      check          Check hyprlua configuration
-      init           Initialize with auto-detection
-      config [path]  View/parse configuration file
+    Simple tool to update old Hyprland configs to the new Lua format.
+    Updated configs will be placed in the same directory as the original config by default, but you can specify a custom output file with the --output option.
     
     Options:
       --help, -h     Show this help message
       --version, -v  Show version information
+      --config, -c   Path to config directory (uses standard ~/.config if omitted)
+      --output, -o   Custom output file (defaults to placing files in the same directory as the original config)
     ''')
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='HyprLua Configuration Manager')
-    parser.add_argument('--help', '-h', action='store_true', help='Show help and exit')
+    parser = argparse.ArgumentParser(description='Simple tool to update old Hyprland configs to the new Lua format.\nUpdated configs will be placed in the same directory as the original config by default, but you can specify a custom output file with the --output option.', add_help=False)
+    parser.add_argument('-h', '--help', action='store_true', help='Show this help message and exit')
     parser.add_argument('--version', '-v', action='version', version='HyprLua 0.1')
-    parser.add_argument('--config', '-c', dest='config_path', nargs='?', const='.', default=None,
-                       help='Path to config directory (uses current dir if omitted)')
-    parser.add_argument('command', nargs='?', default='check', choices=['check', 'init', 'config'],
-                       help='Command to execute (default: check)')
-    parser.add_argument('config_file', nargs='?', default=None, help='Path to config file (for config command)')
-    
+    parser.add_argument('--config', '-c', dest='config_path', nargs='?',  default=None,
+                        help='Path to config directory (uses standard ~/.config if omitted)')
+    parser.add_argument('--output', '-o',  dest='output_path', nargs='?', default=None,
+                        help='Custom output file (defaults to placing files in the same directory as the original config)')
     return parser.parse_args()
-
-
-def get_conf_dir(config_path=None):
-    """Get configuration directory from CLI argument or auto-detection."""
-    if config_path:
-        conf = config_file.conf(config_path)
-    else:
-        conf = config_file.conf()
-    
-    return conf
-
 
 def main():
     args = parse_arguments()
@@ -51,26 +37,10 @@ def main():
         return
     
     # Get config directory from CLI argument or auto-detect
-    conf = get_conf_dir(config_path=args.config_path)
-    print('Using {} as config directory'.format(conf.conf_dir))
-    
-    if args.command == 'check':
-        parser_args = parser.ConfigParser(config_dir=conf.conf_dir)
-        config = parser_args.parse()
-        print('Configuration loaded successfully!')
-    
-    elif args.command == 'init':
-        print('Initialized using {} as conf dir'.format(conf.conf_dir))
-    
-    elif args.command == 'config':
-        if args.config_file:
-            print('Parsing config from: {}'.format(args.config_file))
-            parser_args = parser.ConfigParser(config_dir=conf.conf_dir)
-            config = parser_args.parse()
-        else:
-            parser_args = parser.ConfigParser(config_dir=conf.conf_dir)
-            config = parser_args.parse()
-            print('Configuration file: {}'.format(config_file.conf().conf_file))
+    conf = config_file.Conf(args.config_path)
+    print('Using {} as Hyprland config directory'.format(conf.conf_dir))
+
+    return
 
 
 if __name__ == '__main__':
